d44ef9665636d624da70bf4e5c58c5d16d527aa9
Dennis Williams <d_williams15@hotmail.com>
2021-08-02 23:38:45 -0500

    Fixed a broken path in require statement

--- src/components/TeamStatusComponent/TeamCharacterStatusComponent/TeamCharacterStatusComponent.svelte
+++ src/components/TeamStatusComponent/TeamCharacterStatusComponent/TeamCharacterStatusComponent.svelte
@@ -1,7 +1,7 @@
 <script>
   import _ from 'lodash';
   import NumericInput from '../../NumericInput.svelte';
-  import magicSpells from '../../../../../configs/magic-spells';
+  import magicSpells from '../../../configs/magic-spells';
   
 	export let character;
   export let onTeamCharacterStatusChange = _.noop;
